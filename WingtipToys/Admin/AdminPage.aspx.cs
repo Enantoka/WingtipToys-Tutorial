d3b9bd94be2e8c70890eb3e490a10fa76617b4cc
Enantoka <enantoka@gmail.com>
2020-06-18 20:51:32 +1000

    Edit AdminPage to add UI and functionality for admin users to add and remove products. Created AddProducts class to handle adding a new product.

--- WingtipToys/Admin/AdminPage.aspx.cs
+++ WingtipToys/Admin/AdminPage.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using WingtipToys.Logic;
 
 namespace WingtipToys.Admin
 {
@@ -11,7 +12,107 @@ namespace WingtipToys.Admin
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            string productAction = Request.QueryString["ProductAction"];
+            if (productAction == "add")
+            {
+                LabelAddStatus.Text = "Product added!";
+            }
 
+            if (productAction == "remove")
+            {
+                LabelRemoveStatus.Text = "Product removed!";
+            }
+        }
+
+        protected void AddProductButton_Click(object sender, EventArgs e)
+        {
+            Boolean fileOk = false;
+            String path = Server.MapPath("~/Catalog/Images/");
+            if (ProductImage.HasFile)
+            {
+                String fileExtension = System.IO.Path.GetExtension(ProductImage.FileName).ToLower();
+                String[] allowedExtensions = { ".gif", ".png", ".jpeg", ".jpg" };
+                for (int i = 0; i < allowedExtensions.Length; i++)
+                {
+                    if (fileExtension == allowedExtensions[i])
+                    {
+                        fileOk = true;
+                    }
+                }
+            }
+
+            if (fileOk)
+            {
+                try
+                {
+                    // Save to Images folder
+                    ProductImage.PostedFile.SaveAs(path + ProductImage.FileName);
+
+                    // Save to Images/Thumbs folder
+                    ProductImage.PostedFile.SaveAs(path + "Thumbs" + ProductImage.FileName);
+                }
+                catch (Exception e)
+                {
+                    LabelAddStatus.Text = e.Message;
+                }
+
+                // Add product data to DB
+                AddProducts products = new AddProducts();
+                bool addSuccess = products.AddProduct(AddProductName.Text, AddProductDescription.Text, AddProductPrice.Text, 
+                                    DropDownAddCategory.SelectedValue, ProductImage.FileName);
+
+                if (addSuccess)
+                {
+                    // Reload the page
+                    string pageUrl = Request.Url.AbsoluteUri.Substring(0, Request.Url.AbsoluteUri.Count() - Request.Url.Query.Count());
+                    Response.Redirect(pageUrl + "?ProductAction=add");
+                }
+                else
+                {
+                    LabelAddStatus.Text = "Unable to add new product to database.";
+                }
+            }
+            else
+            {
+                LabelAddStatus.Text = "Unable to accept file type.";
+            }
+        }
+
+        public IQueryable GetCatagories()
+        {
+            var _db = new WingtipToys.Models.ProductContext();
+            IQueryable query = _db.Categories;
+            return query;
+        }
+
+        public IQueryable GetProducts()
+        {
+            var _db = new WingtipToys.Models.ProductContext();
+            IQueryable query = _db.Products;
+            return query;
+        }
+
+        protected void RemoveProductButton_Click(object sender, EventArgs e)
+        {
+            using (var _db = new WingtipToys.Models.ProductContext())
+            {
+                int productId = Convert.ToInt16(DropDownRemoveProduct.SelectedValue);
+                var myItem = (from c in _db.Products where c.ProductID == productId select c).FirstOrDefault();
+                
+                if (myItem != null)
+                {
+                    _db.Products.Remove(myItem);
+                    _db.SaveChanges();
+
+                    // Reload the page
+                    string pageUrl = Request.Url.AbsoluteUri.Substring(0, Request.Url.AbsoluteUri.Count() - Request.Url.Query.Count());
+                    Response.Redirect(pageUrl + "?ProductAction=remove");
+                }
+                else
+                {
+                    LabelRemoveStatus.Text = "Unable to locate product.";
+                }
+            }
         }
     }
 }
